46d2e208ce87c76c45c3eececeab0aeacf3cc37d
zielaskowski <michal.zielaskowski@gmail.com>
2020-10-15 17:48:27 +0200

    deleting old files

--- requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pipreqs --no-pin --force
-packages=$(cat requirements.txt | paste -s -d,)
-rm ./requirements.txt
-pipdeptree -p $packages > requirements.txt
-pipdeptree -p $packages --graph-output jpg > requirements.jpg
-
-
